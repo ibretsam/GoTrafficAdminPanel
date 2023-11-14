faff00e8ddc6b7d7ec49608772108d713708cafc
Khanh Le <masterbruculous@gmail.com>
2023-11-14 10:48:47 +0700

    update CarItem

--- src/components/CarItem.tsx
+++ src/components/CarItem.tsx
@@ -5,11 +5,57 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-export const CarItem: React.FC<Car> = (car) => {
-  const images = JSON.parse(car.image);
+const Line: React.FC<{ title: string; value: string }> = ({ title, value }) => {
   return (
-    <div className="flex flex-col bg-white shadow-md rounded-lg p-6">
-      <div style={{ width: "82rem" }}>
+    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+      <dt className="text-sm font-medium leading-6 text-gray-900">{title}</dt>
+      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+        {value}
+      </dd>
+    </div>
+  );
+};
+
+export const CarItem: React.FC<{ car: Car; isApprovalMode: boolean }> = ({
+  car,
+  isApprovalMode,
+}) => {
+  let images = [];
+  if (car.image) {
+    try {
+      images = JSON.parse(car.image);
+    } catch (error) {
+      console.log(error);
+      images.push(car.image);
+    }
+  }
+
+  const handleApprove = async (isApproval: boolean) => {
+    let url = `http://103.57.129.166:3000/car/api/${
+      isApproval ? "browse" : "refuse-car"
+    }?idCar=${car.id}`;
+    console.log(url);
+    if (isApproval) {
+      await fetch(url, {
+        method: "PUT",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.result === true) {
+            window.location.reload();
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Có lỗi xảy ra");
+        });
+    }
+  };
+
+  return (
+    <div className="flex items-stretch flex-col bg-white shadow-md rounded-lg p-6 ml-64">
+      <div style={{ width: "50rem" }} className="flex self-center">
         <Swiper
           slidesPerView={1}
           spaceBetween={30}
@@ -35,14 +81,36 @@ export const CarItem: React.FC<Car> = (car) => {
         </Swiper>
       </div>
       <div className="mt-4">
-        <h2 className="text-2xl font-bold">{car.name}</h2>
-        <p className="text-gray-600">{car.yearOfManufacture}</p>
-        <p className="text-gray-600">{car.locationCar}</p>
-        <p className="text-gray-600">{car.city}</p>
-        <p className="text-gray-600">{car.district}</p>
-        <p className="text-gray-600">{car.ward}</p>
-        <p className="text-gray-600">{car.description}</p>
-        <p className="text-gray-600">{car.price}</p>
+        <div className="px-4 sm:px-0 flex items-center justify-between">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            {car.name}
+          </h2>
+          {isApprovalMode && (
+            <div>
+              <button
+                className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 mr-4 transition duration-200"
+                onClick={() => handleApprove(true)}
+              >
+                Duyệt
+              </button>
+              <button
+                className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 transition duration-200"
+                onClick={() => handleApprove(false)}
+              >
+                Từ chối
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="mt-6 border-t border-gray-100">
+          <Line title="Năm sản xuất" value={car.yearOfManufacture.toString()} />
+          <Line title="Đia điểm" value={car.locationCar} />
+          <Line title="Thành phố" value={car.city} />
+          <Line title="Quận/Huyện" value={car.district} />
+          <Line title="Phường/Xã" value={car.ward} />
+          <Line title="Mô tả" value={car.description} />
+          <Line title="Giá" value={car.price.toString()} />
+        </div>
       </div>
     </div>
   );
