0ae569363d7adb4fcda1d57b40edc5febb54f013
Khanh Le <masterbruculous@gmail.com>
2023-12-19 01:56:43 +0700

    Router and Provider update

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Login from "./pages/Login";
 import CarApproval from "./pages/CarApproval";
 import CarList from "./pages/CarList";
 import UserList from "./pages/UserList";
+import SendNotification from "./pages/SendNotification";
+import MoneyWithdrawal from "./pages/MoneyWithdrawal";
 
 const MainLayout = () => (
   <div className="flex">
@@ -24,6 +26,8 @@ function App() {
           <Route path="/car-approval" element={<CarApproval />} />
           <Route path="/car-list" element={<CarList />} />
           <Route path="/user-list" element={<UserList />} />
+          <Route path="/send-notification" element={<SendNotification />} />
+          <Route path="/money-withdrawal" element={<MoneyWithdrawal />} />
         </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
