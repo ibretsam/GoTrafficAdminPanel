eec95881386be10f6cfa35374dff0b70ec4fa6e8
Khanh Le <masterbruculous@gmail.com>
2023-12-19 01:56:12 +0700

    environment update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    "node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
+});
+
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "node_modules/flowbite-react/lib/esm/**/*.js",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require("@tailwindcss/forms"),
+//     require("flowbite/plugin"),
+//     // ...
+//   ],
+// };
